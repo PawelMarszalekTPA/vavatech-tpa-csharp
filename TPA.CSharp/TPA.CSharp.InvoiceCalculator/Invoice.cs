10cc0cce619700603820da294549bfc355639f9f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-08-04 10:47:16 +0200

    Klasy i obiekty

---
+++ TPA.CSharp/TPA.CSharp.InvoiceCalculator/Invoice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPA.CSharp.InvoiceCalculator
+{
+    public class Invoice
+    {
+        // pole
+        public string number;
+        public DateTime createdDate;
+        public Customer customer;
+        public DateTime dueDate;
+    }
+
+    public class Customer
+    {
+        public string name;
+        public string street;
+        public string postCode;
+        public string city;
+    }
+}
---
+++ TPA.CSharp/TPA.CSharp.InvoiceCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace TPA.CSharp.InvoiceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Customer customer = new Customer();
+            customer.name = "Vavatech Sp. z o.o.";
+            customer.street = "Olesińska 18";
+            customer.postCode = "00-950";
+            customer.city = "Warszawa";
+
+            Invoice invoice = new Invoice();
+            invoice.number = "FV 100/2020";
+            invoice.customer = customer;
+            invoice.createdDate = DateTime.Now;
+            invoice.dueDate = DateTime.Today.AddDays(14);
+
+            Console.WriteLine(invoice.number);
+            Console.WriteLine(invoice.createdDate);
+            Console.WriteLine(invoice.customer.name);
+            Console.WriteLine($"{invoice.customer.street} {invoice.customer.postCode} {invoice.customer.city}");
+        }
+    }
+}
