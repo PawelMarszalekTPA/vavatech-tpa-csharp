7d9961e9f8663d867279194d81fa077dad7bc3d2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-08-04 11:52:00 +0200

    Konstruktor

--- TPA.CSharp/TPA.CSharp.InvoiceCalculator/Invoice.cs
+++ TPA.CSharp/TPA.CSharp.InvoiceCalculator/Invoice.cs
@@ -11,13 +11,43 @@ namespace TPA.CSharp.InvoiceCalculator
         public DateTime createdDate;
         public Customer customer;
         public DateTime dueDate;
+
+        // Konstruktor - metoda uruchamiana automatycznie podczas tworzenia instancji obiektu
+        public Invoice(string number, Customer customer)
+        {
+            createdDate = DateTime.Now;
+            dueDate = createdDate.AddDays(14);
+
+            this.number = number;
+            this.customer = customer;
+
+        }
     }
 
     public class Customer
     {
         public string name;
+
+        public Address invoiceAddress;
+        public Address postAddress;
+
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    public class Address
+    {
         public string street;
         public string postCode;
         public string city;
+
+        public Address(string street, string postCode, string city)
+        {
+            this.street = street;
+            this.postCode = postCode;
+            this.city = city;
+        }
     }
 }
--- TPA.CSharp/TPA.CSharp.InvoiceCalculator/Program.cs
+++ TPA.CSharp/TPA.CSharp.InvoiceCalculator/Program.cs
@@ -7,24 +7,39 @@ namespace TPA.CSharp.InvoiceCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateInvoiceTest2();
+        }
 
-            Customer customer = new Customer();
-            customer.name = "Vavatech Sp. z o.o.";
-            customer.street = "Olesińska 18";
-            customer.postCode = "00-950";
-            customer.city = "Warszawa";
+        private static void CreateInvoiceTest1()
+        {
+            Customer customer = new Customer("Vavatech Sp. z o.o.");
+            customer.invoiceAddress = new Address("Olesińska 18", "00-950", "Warszawa");
+            customer.postAddress = new Address("Dworcowa 1", "00-001", "Warszawa");
 
-            Invoice invoice = new Invoice();
-            invoice.number = "FV 100/2020";
-            invoice.customer = customer;
+            Invoice invoice = new Invoice("FV 100/2020", customer);
+            //invoice.number = "FV 100/2020";
+            // invoice.customer = customer;
             invoice.createdDate = DateTime.Now;
             invoice.dueDate = DateTime.Today.AddDays(14);
 
             Console.WriteLine(invoice.number);
             Console.WriteLine(invoice.createdDate);
             Console.WriteLine(invoice.customer.name);
-            Console.WriteLine($"{invoice.customer.street} {invoice.customer.postCode} {invoice.customer.city}");
+            Console.WriteLine($"{invoice.customer.invoiceAddress.street} {invoice.customer.invoiceAddress.postCode} {invoice.customer.invoiceAddress.city}");
+        }
+
+        private static void CreateInvoiceTest2()
+        {
+            Customer customer = new Customer("Vavatech Sp. z o.o.");
+            customer.invoiceAddress = new Address("Olesińska 18", "00-950", "Warszawa");
+            customer.postAddress = new Address("Dworcowa 1", "00-001", "Warszawa");
+
+            Invoice invoice = new Invoice("FV 100/2020", customer);
+
+            Console.WriteLine(invoice.number);
+            Console.WriteLine(invoice.createdDate);
+            Console.WriteLine(invoice.customer.name);
+            Console.WriteLine($"{invoice.customer.invoiceAddress.street} {invoice.customer.invoiceAddress.postCode} {invoice.customer.invoiceAddress.city}");
         }
     }
 }
