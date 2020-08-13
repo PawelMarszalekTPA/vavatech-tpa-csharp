0b1cd20a7f73f65a0ce465429ebf3187ddd84ec0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-08-13 11:53:25 +0200

    Warunek if

---
+++ TPA.CSharp/TPA.CSharp.Fundamentals/11_Conditions/ConditionsTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPA.CSharp.Fundamentals._11_Conditions
+{
+    public class ConditionsTest
+    {
+        public static void Test()
+        {
+            int amount = 560;
+
+            // if
+
+            // ponizej 500
+
+            // powyzej 500
+
+            string message;
+
+            if (amount<500)
+            {
+               message = "Poniżej 500";
+            }
+            else
+            {
+                message = "Powyżej 500";
+            }
+
+            Console.WriteLine(message);
+
+            // skrócony zapis if
+            // warunek ? true : false 
+            // JEŻELI(wyrażenie;true;false)
+            string message2 = amount < 500 ? "Poniżej 500" : "Powyżej 500";
+
+            Console.WriteLine(message2);
+
+        }
+    }
+}
--- TPA.CSharp/TPA.CSharp.Fundamentals/Program.cs
+++ TPA.CSharp/TPA.CSharp.Fundamentals/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using TPA.CSharp.Fundamentals._09_Casting;
 using TPA.CSharp.Fundamentals._10_Strings;
+using TPA.CSharp.Fundamentals._11_Conditions;
 using TPA.CSharp.Fundamentals.Inheritance;
 
 namespace TPA.CSharp.Fundamentals
@@ -10,6 +11,9 @@ namespace TPA.CSharp.Fundamentals
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            ConditionsTest.Test();
 
             StringsTest.Test();
 
